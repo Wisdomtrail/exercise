7d04a1d75576391ee034dbaa9c8eb126558103ce
sunday <117924550+sunepa01@users.noreply.github.com>
2023-04-14 15:12:03 +0100

    Delete Untitled3 directory

--- Untitled3/Alphabet.js
+++
@@ -1,5 +0,0 @@
-let string = "SEMICOLON";
-var arr = string.split('') 
-arr.forEach(element => {
-    console.log(element+"\n")
-});
--- Untitled3/divisible.js
+++
@@ -1,7 +0,0 @@
-let sum = 0;
-for (i = 1; i <= 30; i++) {
-    if (i%3 == 0){
-        sum+=i;
-    }
-}
-console.log(sum);
